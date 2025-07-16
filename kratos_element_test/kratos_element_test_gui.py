496b2cee953528967a9690cdbd69f37616c0adce
Jonathan Nuttall <jonathan.nuttall@deltares.nl>
2025-07-16 13:07:52 +0200

    Added libraries + path settings to read plaxis UDSMs

--- kratos_element_test/kratos_element_test_gui.py
+++ kratos_element_test/kratos_element_test_gui.py
@@ -4,15 +4,20 @@
 
 import os
 import sys
+
 from kratos_element_test.ui_menu import create_menu
 
+if sys.platform == "win32" and hasattr(os, "add_dll_directory"):
+    print("Adding Directory")
+    dll_directory = os.path.abspath("./libs")
+    # Add the dll_directory to the PATH environment variable temporarily
+    os.environ['PATH'] = dll_directory + ';' + os.environ.get('PATH', '')
+    # Now you can load DLLs from that directory in this Python session or subprocesses
+    print("Temporary PATH:", os.environ['PATH'])
 
 def main():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     os.chdir(current_dir)
-    element_test_path = os.path.join(current_dir, "applications", "GeoMechanicsApplication", "python_scripts", "element_test")
-    if element_test_path not in sys.path:
-        sys.path.insert(0, element_test_path)
 
     try:
         create_menu()
