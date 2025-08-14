3602582914044300e58f4fc174f180a7266e8ecc
Marjan Fathian <marjan.fathian@deltares.nl>
2025-08-14 14:17:35 +0200

    moved kratos_element_test_gui.py to core/pipeline directory

--- kratos_element_test/core/__init__.py
+++ kratos_element_test/core/__init__.py
@@ -3,3 +3,5 @@ from .io.mdpa_editor import MdpaEditor
 from .io.project_parameter_editor import ProjectParameterEditor
 from .io.material_editor import MaterialEditor
 from .io.gid_output_reader import GiDOutputFileReader, read_coordinates_from_post_msh_file
+from .pipeline.run_simulation import run_simulation
+from .pipeline.generic_test_runner import GenericTestRunner
