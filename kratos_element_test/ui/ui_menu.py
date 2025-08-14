16cd27b381b92f609809a8e9bb5581e03933224d
Marjan Fathian <marjan.fathian@deltares.nl>
2025-08-14 14:56:55 +0200

    moved ui_udsm_parser.py to ui directory

--- kratos_element_test/ui/ui_menu.py
+++ kratos_element_test/ui/ui_menu.py
@@ -8,7 +8,7 @@ from tkinter import filedialog, messagebox, ttk, scrolledtext, Menu
 from platformdirs import user_data_dir
 from pathlib import Path
 from kratos_element_test.ui.ui_builder import GeotechTestUI
-from kratos_element_test.ui_udsm_parser import udsm_parser
+from kratos_element_test.ui.ui_udsm_parser import udsm_parser
 from kratos_element_test.ui_labels import (APP_TITLE, APP_VERSION, APP_NAME, APP_AUTHOR, SELECT_UDSM, LINEAR_ELASTIC,
                                            HELP_MENU_FONT, DEFAULT_TKINTER_DPI)
 
