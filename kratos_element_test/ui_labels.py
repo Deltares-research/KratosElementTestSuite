0de7d42ddde0b5550899253227b0eaf7522e5444
Marjan Fathian <marjan.fathian@deltares.nl>
2025-08-14 15:08:54 +0200

    removed plotter labels from the ui_labels.py

--- kratos_element_test/ui_labels.py
+++ kratos_element_test/ui_labels.py
@@ -2,11 +2,12 @@
 # This is a prototype version
 # Contact kratos@deltares.nl
 
+
 #-----------------------UI labels---------------------------
 
 # Application data
 APP_TITLE = "Deltares Soil Element Test Suite"
-APP_VERSION = "Version 0.1.2 ~ Alpha Release"
+APP_VERSION = "Version 0.1.3 ~ Alpha Release"
 APP_NAME = "SoilElementSuite"
 APP_AUTHOR = "Deltares"
 
@@ -31,37 +32,9 @@ WITHOUT_UNIT_LABEL = ""
 SELECT_UDSM = "Select UDSM File"
 LINEAR_ELASTIC = "Linear Elastic Model"
 
-
-#-----------------------Plot labels--------------------------
-
-# Axis labels
-SIGMA1_LABEL = "σ₁ (Principal Stress 1) [kN/m²]"
-SIGMA3_LABEL = "σ₃ (Principal Stress 3) [kN/m²]"
-SIGMA1_SIGMA3_DIFF_LABEL = "|σ₁ - σ₃| [kN/m²]"
-VERTICAL_STRAIN_LABEL = "εᵧᵧ (Vertical Strain) [-]"
-VOLUMETRIC_STRAIN_LABEL = "εᵥ (Volumetric Strain) [-]"
-SHEAR_STRAIN_LABEL = "γₓᵧ (Shear Strain) [-]"
-SHEAR_STRESS_LABEL = "τₓᵧ (Shear Stress) [kN/m²]"
-EFFECTIVE_STRESS_LABEL = "σ' (Effective Stress) [kN/m²]"
-MOBILIZED_SHEAR_STRESS_LABEL = "τ (Mobilized Shear Stress) [kN/m²]"
-P_STRESS_LABEL = "p' (Mean Effective Stress) [kN/m²]"
-Q_STRESS_LABEL = "q (Deviatoric Stress) [kN/m²]"
-
-# Titles
-TITLE_SIGMA1_VS_SIGMA3 = "σ₁ vs σ₃"
-TITLE_DIFF_PRINCIPAL_SIGMA_VS_STRAIN = "|σ₁ - σ₃| vs εᵧᵧ"
-TITLE_VOL_VS_VERT_STRAIN = "εᵥ vs εᵧᵧ"
-TITLE_MOHR = "Mohr-Coulomb"
-TITLE_P_VS_Q = "p' vs q"
-TITLE_SHEAR_VS_STRAIN = "τₓᵧ vs εₓᵧ"
-
-# Legends
-LEGEND_MC = "Mohr-Coulomb"
-LEGEND_MC_FAILURE = "Failure Criterion: τ = σ' tan(φ°) + c'"
-
 # Font labels
 HELP_MENU_FONT = "Segoe UI"
 INPUT_SECTION_FONT = "Arial"
 
-# Default values
+# Default tkinter values
 DEFAULT_TKINTER_DPI = 72
