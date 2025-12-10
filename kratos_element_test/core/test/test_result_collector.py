90c6ce6ff1f88a913cd47f12079d82508a8bf6a5
Richard Faasse <richard.faasse@deltares.nl>
2025-12-10 15:42:20 +0100

    Made test parameterized to be able to add other variables quickly

--- kratos_element_test/core/test/test_result_collector.py
+++ kratos_element_test/core/test/test_result_collector.py
@@ -2,6 +2,7 @@ import os
 import unittest
 from pathlib import Path
 import numpy as np
+from parameterized import parameterized
 
 from kratos_element_test.core.pipeline.result_collector import ResultCollector
 
@@ -28,15 +29,17 @@ class ResultCollectorTest(unittest.TestCase):
         self.assertIn("sigma_yy", results)
         self.assertIn("time_steps", results)
 
-    def test_yy_strain_values(self):
+    @parameterized.expand([
+        ["yy_strain", [0.0, -0.05, -0.1, -0.15, -0.2]]
+    ])
+    def test_values(self, variable_name, expected_values):
         file_path = Path(os.path.dirname(__file__))
         test_path = file_path / "output.post.res"
         collector = ResultCollector(
             [test_path], material_parameters=[], cohesion_phi_indices=None
         )
         results = collector.collect_results()
-        expected_yy_strain = [0.0, -0.05, -0.1, -0.15, -0.2]
-        np.testing.assert_array_almost_equal(results["yy_strain"], expected_yy_strain)
+        np.testing.assert_array_almost_equal(results[variable_name], expected_values)
 
 
 if __name__ == "__main__":
