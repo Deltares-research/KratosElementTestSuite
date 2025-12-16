163540b7857bed6df75953629ade1dfed7907c04
Richard Faasse <richard.faasse@deltares.nl>
2025-12-16 15:45:53 +0100

    Added updating the view based on the model

--- experiment_frameworks/kivy/controller.py
+++ experiment_frameworks/kivy/controller.py
@@ -5,11 +5,22 @@ class Controller:
 
         view.button.bind(on_press=self.on_generate_graph)
 
-
     def start(self):
         self.view.run()
+        self.update_fields_based_on_model()
+
+    def update_fields_based_on_model(self):
+        self.view.amplitude_input.text = str(self.model.amplitude)
+        self.view.frequency_input.text = str(self.model.frequency)
+        self.view.phase_input.text = str(self.model.phase)
 
     def on_generate_graph(self, instance):
+        self.update_model_based_on_inputs()
+        self.update_fields_based_on_model()
+        x_values, y_values = self.model.generate_graph_data()
+        self.view.display_data(x_values, y_values)
+
+    def update_model_based_on_inputs(self):
         amplitude = self.view.amplitude_input.text
         self.model.set_amplitude(amplitude)
 
@@ -18,6 +29,3 @@ class Controller:
 
         phase = self.view.phase_input.text
         self.model.set_phase(phase)
-
-        x_values, y_values = self.model.generate_graph_data()
-        self.view.display_data(x_values, y_values)
