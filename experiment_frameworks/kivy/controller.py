cff0229c8b06ca3a036acbb853b03459992e7a91
Richard Faasse <richard.faasse@deltares.nl>
2025-12-16 15:58:08 +0100

    Changed the update to trigger when the text changes

--- experiment_frameworks/kivy/controller.py
+++ experiment_frameworks/kivy/controller.py
@@ -4,10 +4,34 @@ class Controller:
         self.model = model
 
         view.button.bind(on_press=self.on_generate_graph)
+        view.amplitude_input.bind(text=self.on_amplitude_change)
+        view.frequency_input.bind(text=self.on_frequency_change)
+        view.phase_input.bind(text=self.on_phase_change)
 
     def start(self):
         self.update_fields_based_on_model()
         self.view.run()
+
+    def on_amplitude_change(self, instance, value):
+        try:
+            float(value)
+        except ValueError:
+            return
+        self.model.amplitude = float(value)
+
+    def on_frequency_change(self, instance, value):
+        try:
+            float(value)
+        except ValueError:
+            return
+        self.model.frequency = float(value)
+
+    def on_phase_change(self, instance, value):
+        try:
+            float(value)
+        except ValueError:
+            return
+        self.model.phase = float(value)
 
     def update_fields_based_on_model(self):
         self.view.amplitude_input.text = str(self.model.amplitude)
@@ -15,8 +39,6 @@ class Controller:
         self.view.phase_input.text = str(self.model.phase)
 
     def on_generate_graph(self, instance):
-        self.update_model_based_on_inputs()
-        self.update_fields_based_on_model()
         x_values, y_values = self.model.generate_graph_data()
         self.view.display_data(x_values, y_values)
 
