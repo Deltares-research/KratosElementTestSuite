86279ebaff9594a8fc55081d0ba5cff1ac110a10
Richard Faasse <56549273+rfaasse@users.noreply.github.com>
2025-12-18 10:14:11 +0100

    Separate running and obtaining results (#31)
    - Creating a new class for collecting the data: ResultCollector
    - Moving functionality from the GenericTestRunner and RunSimulation classes related to collecting results to the ResultCollector
    - Make two separate calls in the RunSimulation, such that the controller can call them separately
    - Moved the render functionality to the controller, such that the model/core doesn't have to know about plotting (small bonus)

--- kratos_element_test/view/ui_constants.py
+++ kratos_element_test/view/ui_constants.py
@@ -7,7 +7,7 @@
 
 # Application data
 APP_TITLE = "Deltares Soil Element Test Suite"
-APP_VERSION = "Version 0.1.5 ~ Alpha Release"
+APP_VERSION = "Version 0.1.6 ~ Alpha Release"
 APP_NAME = "SoilElementSuite"
 APP_AUTHOR = "Deltares"
 
