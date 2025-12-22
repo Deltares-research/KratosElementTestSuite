e1136f1bfa29d93d5860008e112c7c6c66232d69
Richard Faasse <richard.faasse@deltares.nl>
2025-12-22 14:33:27 +0100

    Reformatted soil test input manager

--- kratos_element_test/model/soil_test_input_manager.py
+++ kratos_element_test/model/soil_test_input_manager.py
@@ -14,19 +14,28 @@ from kratos_element_test.view.ui_constants import (
 class SoilTestInputManager:
     def __init__(self):
         self.input_data = {}
-        self.input_data[TRIAXIAL] = TriaxialAndShearSimulationInputs(test_type=TEST_NAME_TO_TYPE.get(TRIAXIAL), maximum_strain=20.0,
-                                                                     initial_effective_cell_pressure=100.0,
-                                                                     number_of_steps=100,
-                                                                     duration=1.0)
+        self.input_data[TRIAXIAL] = TriaxialAndShearSimulationInputs(
+            test_type=TEST_NAME_TO_TYPE.get(TRIAXIAL),
+            maximum_strain=20.0,
+            initial_effective_cell_pressure=100.0,
+            number_of_steps=100,
+            duration=1.0,
+        )
 
-        self.input_data[DIRECT_SHEAR] = TriaxialAndShearSimulationInputs(test_type=TEST_NAME_TO_TYPE.get(TRIAXIAL), maximum_strain=20.0,
-                                                                         initial_effective_cell_pressure=100.0,
-                                                                         number_of_steps=100,
-                                                                         duration=1.0)
+        self.input_data[DIRECT_SHEAR] = TriaxialAndShearSimulationInputs(
+            test_type=TEST_NAME_TO_TYPE.get(TRIAXIAL),
+            maximum_strain=20.0,
+            initial_effective_cell_pressure=100.0,
+            number_of_steps=100,
+            duration=1.0,
+        )
 
         default_strain_increments = []
         for i in range(5):
             default_strain_increments.append(
                 StrainIncrement(duration=1.0, strain_increment=0.0, steps=100)
             )
-        self.input_data[CRS] = CRSSimulationInputs(test_type=TEST_NAME_TO_TYPE.get(CRS), strain_increments=default_strain_increments)
+        self.input_data[CRS] = CRSSimulationInputs(
+            test_type=TEST_NAME_TO_TYPE.get(CRS),
+            strain_increments=default_strain_increments,
+        )
