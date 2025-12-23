0add24c9297640fe795cc8a5b56a1a70c91b1848
Richard Faasse <richard.faasse@deltares.nl>
2025-12-23 13:49:21 +0100

    Formatting

--- kratos_element_test/controller/soil_test_input_controller.py
+++ kratos_element_test/controller/soil_test_input_controller.py
@@ -61,25 +61,53 @@ class SoilTestInputController:
     def remove_last_crs_strain_increment(self):
         self._soil_test_input_manager.remove_last_crs_strain_increment()
 
+    def bind_test_input_fields_to_update_functions(
+        self, widgets, test_type: str
+    ) -> None:
+        widgets[INIT_PRESSURE_LABEL].bind(
+            "<FocusOut>",
+            lambda e: self.update_init_pressure(
+                new_pressure=float(widgets[INIT_PRESSURE_LABEL].get()),
+                test_type=test_type,
+            ),
+        )
+        widgets[MAX_STRAIN_LABEL].bind(
+            "<FocusOut>",
+            lambda e: self.update_max_strain(
+                new_strain=float(widgets[MAX_STRAIN_LABEL].get()), test_type=test_type
+            ),
+        )
+        widgets[NUM_STEPS_LABEL].bind(
+            "<FocusOut>",
+            lambda e: self.update_num_steps(
+                new_steps=int(widgets[NUM_STEPS_LABEL].get()), test_type=test_type
+            ),
+        )
+        widgets[DURATION_LABEL].bind(
+            "<FocusOut>",
+            lambda e: self.update_duration(
+                new_duration=float(widgets[DURATION_LABEL].get()), test_type=test_type
+            ),
+        )
 
-    def bind_test_input_fields_to_update_functions(self, widgets, test_type: str) -> None:
-        widgets[INIT_PRESSURE_LABEL].bind("<FocusOut>", lambda e: self.update_init_pressure(
-            new_pressure=float(widgets[INIT_PRESSURE_LABEL].get()), test_type=test_type
-        ))
-        widgets[MAX_STRAIN_LABEL].bind("<FocusOut>", lambda e: self.update_max_strain(
-            new_strain=float(widgets[MAX_STRAIN_LABEL].get()), test_type=test_type
-        ))
-        widgets[NUM_STEPS_LABEL].bind("<FocusOut>", lambda e: self.update_num_steps(
-            new_steps=int(widgets[NUM_STEPS_LABEL].get()), test_type=test_type
-        ))
-        widgets[DURATION_LABEL].bind("<FocusOut>", lambda e: self.update_duration(
-            new_duration=float(widgets[DURATION_LABEL].get()), test_type=test_type
-        ))
-
-    def bind_crs_test_input_row_to_update_functions(self, row, current_index: int) -> None:
-        row[DURATION_LABEL].bind("<FocusOut>", lambda e, idx=current_index: self.update_crs_duration(
-            new_duration=float(row[DURATION_LABEL].get()), index = idx))
-        row[STRAIN_INCREMENT_LABEL].bind("<FocusOut>", lambda e, idx=current_index: self.update_crs_strain_increment(
-            new_strain_increment=float(row[STRAIN_INCREMENT_LABEL].get()), index = idx))
-        row[STEPS_LABEL].bind("<FocusOut>", lambda e, idx=current_index: self.update_crs_number_of_steps(
-            new_steps=int(row[STEPS_LABEL].get()), index = idx))
+    def bind_crs_test_input_row_to_update_functions(
+        self, row, current_index: int
+    ) -> None:
+        row[DURATION_LABEL].bind(
+            "<FocusOut>",
+            lambda e, idx=current_index: self.update_crs_duration(
+                new_duration=float(row[DURATION_LABEL].get()), index=idx
+            ),
+        )
+        row[STRAIN_INCREMENT_LABEL].bind(
+            "<FocusOut>",
+            lambda e, idx=current_index: self.update_crs_strain_increment(
+                new_strain_increment=float(row[STRAIN_INCREMENT_LABEL].get()), index=idx
+            ),
+        )
+        row[STEPS_LABEL].bind(
+            "<FocusOut>",
+            lambda e, idx=current_index: self.update_crs_number_of_steps(
+                new_steps=int(row[STEPS_LABEL].get()), index=idx
+            ),
+        )
