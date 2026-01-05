254c7684a18b33c52746c036e4a1931759734c4c
Richard Faasse <richard.faasse@deltares.nl>
2026-01-05 10:23:14 +0100

    Added unit/integration test for running a linear elastic triaxial sim

---
+++ kratos_element_test/model/test/test_main_model.py
@@ -0,0 +1,22 @@
+import unittest
+
+from kratos_element_test.model.main_model import MainModel
+from kratos_element_test.model.models import MohrCoulombOptions
+
+
+class MainModelTest(unittest.TestCase):
+    def test_running_a_linear_elastic_triaxial_simulation(self):
+        model = MainModel(logger=lambda msg, level: None)
+
+        material_parameters = [9e5, 0.3]
+
+        model.run_simulation(
+            "drained", None, 0, MohrCoulombOptions(), material_parameters
+        )
+
+        results = model.get_latest_results()
+        self.assertIsNotNone(results)
+
+
+if __name__ == "__main__":
+    unittest.main()
