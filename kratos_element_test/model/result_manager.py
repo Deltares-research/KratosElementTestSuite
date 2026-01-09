0a686df04308b6e361a2d493b7281b71904b42a7
Richard Faasse <richard.faasse@deltares.nl>
2026-01-09 09:24:00 +0100

    Renamed variable to reflect that the results are the sim results
    This helps in adding 'gef' or other results here later

--- kratos_element_test/model/result_manager.py
+++ kratos_element_test/model/result_manager.py
@@ -1,13 +1,13 @@
 class ResultManager:
     def __init__(self, current_test_getter):
-        self._results = {}
+        self._simulation_results = {}
         self._current_test_getter = current_test_getter
 
     def get_results(self):
-        return self._results.get(self._current_test_getter(), {})
+        return self._simulation_results.get(self._current_test_getter(), {})
 
     def set_results(self, expected_results):
-        self._results[self._current_test_getter()] = expected_results
+        self._simulation_results[self._current_test_getter()] = expected_results
 
     def get_current_test(self):
         return self._current_test_getter()
