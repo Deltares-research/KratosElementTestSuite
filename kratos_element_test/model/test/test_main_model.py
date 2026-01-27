0a0b77dac1ca94c0109c3357a0a24c4c5569abb8
Richard Faasse <richard.faasse@deltares.nl>
2026-01-27 16:17:05 +0100

    Fix test for running a linear elastic triaxial test

--- kratos_element_test/model/test/test_main_model.py
+++ kratos_element_test/model/test/test_main_model.py
@@ -1,21 +1,23 @@
 import unittest
 
 from kratos_element_test.model.main_model import MainModel
-from kratos_element_test.model.material_inputs import MohrCoulombOptions
+from kratos_element_test.model.material_inputs import MohrCoulombOptions, LinearElasticMaterialInputs
 
 
 class MainModelTest(unittest.TestCase):
     def test_running_a_linear_elastic_triaxial_simulation(self):
         model = MainModel(logger=lambda msg, level: None)
 
-        youngs_modulus = 9e5
-        poisson_ratio = 0.3
+        model.get_material_input_manager().set_current_material_type("linear_elastic")
+        model.get_material_input_manager().update_material_parameter_of_current_type("YOUNG_MODULUS", 9e5)
+        model.get_material_input_manager().update_material_parameter_of_current_type("POISSON_RATIO", 0.3)
+
         model.run_simulation(
             model_name="linear elastic model",
             dll_path=None,
             udsm_number=0,
             mohr_coulomb_options=MohrCoulombOptions(),
-            material_parameters=[youngs_modulus, poisson_ratio],
+            material_parameters=[],
         )
 
         results = model.get_latest_results()
