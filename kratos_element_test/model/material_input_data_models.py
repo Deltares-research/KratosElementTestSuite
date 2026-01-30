85fd61a3603d3ee6c7bf21e295bfb0fae5cf2d80
Richard Faasse <richard.faasse@deltares.nl>
2026-01-30 13:35:50 +0100

    Removed unused to_indices function from MohrCoulombOptions

--- kratos_element_test/model/material_input_data_models.py
+++ kratos_element_test/model/material_input_data_models.py
@@ -12,11 +12,6 @@ class MohrCoulombOptions:
     c_index: Optional[int] = None  # 1-based index from the UDSM mapping
     phi_index: Optional[int] = None
 
-    def to_indices(self) -> Optional[Tuple[int, int]]:
-        if not self.enabled or self.c_index is None or self.phi_index is None:
-            return None
-        return self.c_index, self.phi_index
-
 
 @dataclass
 class Parameter:
